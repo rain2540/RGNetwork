a6028f09d26c60eef3a0251b36a65269819af974
Smartech <rain2540@foxmail.com>
2018-12-04 16:54:21 +0800

    feat (Project) : update to Swift 4.2

--- RGNetwork/AppDelegate.swift
+++ RGNetwork/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions
-        launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool
+        launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
     {
         RGNetwork.showActivityIndicator()
         return true
