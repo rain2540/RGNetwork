971398abebb096f99cb774e47838bb833d28f15d
Rain2540 <rain2540@foxmail.com>
2020-03-06 15:30:30 +0800

    chore (project) : add RGBaseRequest

---
+++ RGNetwork/RGNetwork/RGBaseRequest.swift
@@ -0,0 +1,15 @@
+//
+//  RGBaseRequest.swift
+//  RGNetwork
+//
+//  Created by Rain on 2020/3/6.
+//  Copyright © 2020 Smartech. All rights reserved.
+//
+
+import Foundation
+
+class RGBaseRequest {
+    
+    
+    
+}
