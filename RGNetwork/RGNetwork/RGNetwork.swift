8293d2b8743f6c9c2457edb2cef0bde0aa8505fc
Smartech <rain2540@foxmail.com>
2020-09-01 11:09:09 +0800

    docs (RGNetwork) : add comment

--- RGNetwork/RGNetwork/RGNetwork.swift
+++ RGNetwork/RGNetwork/RGNetwork.swift
@@ -231,6 +231,8 @@ fileprivate extension String {
         return self.hasPrefix("http://") || self.hasPrefix("https://")
     }
 
+    /// 如果含有某个后缀，则删除
+    /// - Parameter suffix: 需要删除的后缀
     mutating func rg_removeLast(ifHas suffix: String) {
         if hasSuffix(suffix) {
             removeLast()
