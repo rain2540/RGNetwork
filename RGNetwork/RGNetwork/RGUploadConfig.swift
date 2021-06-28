8cfe27c89c3eb9d2459f91974bc40b0561823ff3
Rain2540 <rain2540@foxmail.com>
2021-06-28 22:14:53 +0800

    chore (project) : add RGUploadConfig

---
+++ RGNetwork/RGNetwork/RGUploadConfig.swift
@@ -0,0 +1,13 @@
+//
+//  RGUploadConfig.swift
+//  RGNetwork
+//
+//  Created by Rain on 2021/6/28.
+//  Copyright © 2021 Smartech. All rights reserved.
+//
+
+import Foundation
+
+struct RGUploadConfig {
+    
+}
