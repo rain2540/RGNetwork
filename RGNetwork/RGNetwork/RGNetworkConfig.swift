b3bb4a4ea5d5cc04ced418b0fdbae11bd128e3e1
Rain2540 <rain2540@foxmail.com>
2021-06-28 22:00:48 +0800

    chore (project) : add RGNetworkConfig

---
+++ RGNetwork/RGNetwork/RGNetworkConfig.swift
@@ -0,0 +1,13 @@
+//
+//  RGNetworkConfig.swift
+//  RGNetwork
+//
+//  Created by Rain on 2021/6/28.
+//  Copyright © 2021 Smartech. All rights reserved.
+//
+
+import Foundation
+
+protocol RGNetworkConfig {
+
+}
