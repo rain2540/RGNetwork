733283fd6ec28b1eb5db2c9c3818a339aeeb7c33
Smartech <rain_2540@foxmail.com>
2021-09-23 09:12:49 +0800

    chore (project) : add RGDownloadRequest

---
+++ RGNetwork/RGNetwork/RGDownloadRequest.swift
@@ -0,0 +1,13 @@
+//
+//  RGDownloadRequest.swift
+//  RGNetwork
+//
+//  Created by RAIN on 2021/9/23.
+//  Copyright © 2021 Smartech. All rights reserved.
+//
+
+import UIKit
+
+class RGDownloadRequest {
+
+}
