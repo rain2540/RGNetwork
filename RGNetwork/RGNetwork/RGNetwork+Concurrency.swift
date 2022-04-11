db824c93781284251be370df3cc5ca26372c6703
Smartech <rain_2540@foxmail.com>
2022-04-11 16:57:38 +0800

    chore (project) : add RGNetwork+Concurrency

---
+++ RGNetwork/RGNetwork/RGNetwork+Concurrency.swift
@@ -0,0 +1,13 @@
+//
+//  RGNetwork+Concurrency.swift
+//  RGNetwork
+//
+//  Created by RAIN on 2022/4/11.
+//  Copyright © 2022 Smartech. All rights reserved.
+//
+
+import Foundation
+
+extension RGNetwork {
+
+}
