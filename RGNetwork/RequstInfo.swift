19cbf49bdd83ffb4b5bd3446729dd1add2741991
Smartech <rain_2540@foxmail.com>
2022-04-11 10:49:55 +0800

    chore (project) : add RequstInfo

---
+++ RGNetwork/RequstInfo.swift
@@ -0,0 +1,13 @@
+//
+//  RequstInfo.swift
+//  RGNetwork
+//
+//  Created by RAIN on 2022/4/11.
+//  Copyright © 2022 Smartech. All rights reserved.
+//
+
+import Foundation
+
+struct RequstInfo {
+
+}
