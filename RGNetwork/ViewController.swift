59c265ba28171657912d0ff0cf25d6c4a32d3436
Smartech <rain_2540@foxmail.com>
2022-04-15 08:46:26 +0800

    feat (Example) : call TaskGroup.start

--- RGNetwork/ViewController.swift
+++ RGNetwork/ViewController.swift
@@ -112,6 +112,8 @@ final class ViewController: UIViewController {
             let json = try JSONSerialization.jsonObject(with: data) as? [String: Any] ?? [:]
             print(json)
             print(response)
+
+            await TaskGroupSample().start()
         }
     }
 
