c23ef0373ffd0d4c668fcd77cd1820aa282762ef
Smartech <rain_2540@foxmail.com>
2022-04-18 15:46:05 +0800

    feat (RGNetwork) : update container view for show / hide indicator

--- RGNetwork/RGNetwork/RGNetwork.swift
+++ RGNetwork/RGNetwork/RGNetwork.swift
@@ -388,7 +388,7 @@ extension RGNetwork {
         text: String = ""
     ) {
         DispatchQueue.main.async {
-            guard let window = UIApplication.shared.keyWindow else { return }
+            guard let window = UIApplication.shared.keySceneWindow else { return }
             let hud = MBProgressHUD.showAdded(to: window, animated: true)
             hud.mode = mode
             hud.label.text = text
@@ -398,7 +398,7 @@ extension RGNetwork {
     /// 隐藏 indicator
     private static func hideIndicator() {
         DispatchQueue.main.async {
-            guard let window = UIApplication.shared.keyWindow else { return }
+            guard let window = UIApplication.shared.keySceneWindow else { return }
             MBProgressHUD.hide(for: window, animated: true)
         }
     }
