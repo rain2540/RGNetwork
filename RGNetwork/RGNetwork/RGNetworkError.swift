f2e596cd56607132b33466061dd2956c9f679850
Smartech <rain_2540@foxmail.com>
2022-05-07 17:15:08 +0800

    style (RGNetwork)Error : update style

--- RGNetwork/RGNetwork/RGNetworkError.swift
+++ RGNetwork/RGNetwork/RGNetworkError.swift
@@ -13,7 +13,7 @@ import Foundation
 /// - wrongURLFormat : 错误的 URL 格式
 enum RGNetworkError: Error {
 
-    /// 错误类型 - 错误的 URL 格式
-    case wrongURLFormat
+  /// 错误类型 - 错误的 URL 格式
+  case wrongURLFormat
 
 }
