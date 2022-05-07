5b6f9f01eb6f5df0ee3ab238fd2500742c98864d
Smartech <rain_2540@foxmail.com>
2022-05-07 17:18:04 +0800

    style (RGDownloadConfig) : update style

--- RGNetwork/RGNetwork/RGDownloadConfig.swift
+++ RGNetwork/RGNetwork/RGDownloadConfig.swift
@@ -11,34 +11,34 @@ import Alamofire
 
 struct RGDownloadConfig: RGNetworkConfig {
 
-    let urlString: String
-    let method: HTTPMethod
-    let headers: HTTPHeaders?
-    let timeoutInterval: TimeInterval
-    let parameters: Parameters?
-    let encoding: ParameterEncoding
-    var isShowLog: Bool
-    let destination: DownloadRequest.Destination?
+  let urlString: String
+  let method: HTTPMethod
+  let headers: HTTPHeaders?
+  let timeoutInterval: TimeInterval
+  let parameters: Parameters?
+  let encoding: ParameterEncoding
+  var isShowLog: Bool
+  let destination: DownloadRequest.Destination?
 
 
-    init(
-        urlString: String,
-        method: HTTPMethod = .get,
-        parameters: Parameters? = nil,
-        encoding: ParameterEncoding = URLEncoding.default,
-        headers: HTTPHeaders? = nil,
-        timeoutInterval: TimeInterval = 30.0,
-        isShowLog: Bool = true,
-        destination: DownloadRequest.Destination? = nil
-    ) {
-        self.urlString          =   urlString
-        self.method             =   method
-        self.parameters         =   parameters
-        self.encoding           =   encoding
-        self.headers            =   headers
-        self.timeoutInterval    =   timeoutInterval
-        self.isShowLog          =   isShowLog
-        self.destination        =   destination
-    }
+  init(
+    urlString: String,
+    method: HTTPMethod = .get,
+    parameters: Parameters? = nil,
+    encoding: ParameterEncoding = URLEncoding.default,
+    headers: HTTPHeaders? = nil,
+    timeoutInterval: TimeInterval = 30.0,
+    isShowLog: Bool = true,
+    destination: DownloadRequest.Destination? = nil
+  ) {
+    self.urlString          =   urlString
+    self.method             =   method
+    self.parameters         =   parameters
+    self.encoding           =   encoding
+    self.headers            =   headers
+    self.timeoutInterval    =   timeoutInterval
+    self.isShowLog          =   isShowLog
+    self.destination        =   destination
+  }
 
 }
