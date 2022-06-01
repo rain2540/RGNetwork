ca387a7fc25edf3ee148d8f43cae20af04db6d14
Smartech <rain_2540@foxmail.com>
2022-06-01 16:46:02 +0800

    refactor (RGNetwork) : move request decodable

--- RGNetwork/RGNetwork/RGNetwork.swift
+++ RGNetwork/RGNetwork/RGNetwork.swift
@@ -101,50 +101,6 @@ extension RGNetwork {
     }
   }
 
-  /// 通用请求方法，用于获取满足 `Decodable` 协议的实体类对象
-  /// - Parameters:
-  ///   - type: 实体对象的类别
-  ///   - config: 网络请求配置信息
-  ///   - queue: 执行请求的队列，默认为 `DispatchQueue.global()`
-  ///   - showIndicator: 是否显示 Indicator，默认为 `false`
-  ///   - success: 请求成功的 Task
-  ///   - failure: 请求失败的 Task
-  public static func requestDecodable<T: Decodable>(
-    of type: T.Type = T.self,
-    config: RGDataRequestConfig,
-    queue: DispatchQueue = DispatchQueue.global(),
-    showIndicator: Bool = false,
-    success: @escaping DecodableSuccess<T>,
-    failure: @escaping DecodableFailure<T>
-  ) {
-    if showIndicator == true {
-      RGNetwork.showIndicator()
-      RGNetwork.showActivityIndicator()
-    }
-
-    queue.async {
-      do {
-        let urlPath = try urlPathString(by: config.urlString)
-
-        let request = AF.request(
-          urlPath,
-          method: config.method,
-          parameters: config.parameters,
-          encoding: config.encoding,
-          headers: config.headers,
-          requestModifier: { urlRequest in
-            urlRequest.timeoutInterval = config.timeoutInterval
-          })
-          .validate(statusCode: 200 ..< 300)
-
-        RGNetwork.responseDecodable(of: type, with: request, config: config, success: success, failure: failure)
-      } catch {
-        dLog(error)
-        RGNetwork.hideIndicator()
-      }
-    }
-  }
-
 
   // MARK: - UploadRequest
 
