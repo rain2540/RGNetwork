0060e20ac453efeb66603456d26eb30a2e900d6d
Smartech <rain_2540@foxmail.com>
2022-06-01 16:30:26 +0800

    chore (project) : add RGNetwork+Decodable.swift

---
+++ RGNetwork/RGNetwork/RGNetwork+Decodable.swift
@@ -0,0 +1,13 @@
+//
+//  RGNetwork+Decodable.swift
+//  RGNetwork
+//
+//  Created by RAIN on 2022/6/1.
+//  Copyright © 2022 Smartech. All rights reserved.
+//
+
+import Foundation
+
+extension RGNetwork {
+
+}
