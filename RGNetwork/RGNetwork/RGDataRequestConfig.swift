228827c3a11144d8414a0f716562a66608c45594
Smartech <rain_2540@foxmail.com>
2022-06-02 13:04:12 +0800

    feat (RGDataRequestConfig) : update access control

--- RGNetwork/RGNetwork/RGDataRequestConfig.swift
+++ RGNetwork/RGNetwork/RGDataRequestConfig.swift
@@ -9,7 +9,7 @@
 import Foundation
 import Alamofire
 
-struct RGDataRequestConfig: RGNetworkConfig {
+public struct RGDataRequestConfig: RGNetworkConfig {
 
   let urlString: String
   let method: HTTPMethod
