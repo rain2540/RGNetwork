10c14b2805db91b047dce0dbd6a945292b9d9aac
Smartech <rain_2540@foxmail.com>
2022-06-02 11:39:37 +0800

    chore (project) : add extension of Session

---
+++ RGNetwork/RGNetwork/Session+RGNetwork.swift
@@ -0,0 +1,16 @@
+//
+//  Session+RGNetwork.swift
+//  RGNetwork
+//
+//  Created by RAIN on 2022/6/2.
+//  Copyright © 2022 Smartech. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+extension Session {
+
+
+
+}
