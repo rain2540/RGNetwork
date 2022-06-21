59000b9907556c7b87b1bdfab0626358c5637744
Smartech <rain_2540@foxmail.com>
2022-06-21 09:24:43 +0800

    chore (project) : add RGUploadRequest

---
+++ RGNetwork/RGNetwork/RGUploadRequest.swift
@@ -0,0 +1,13 @@
+//
+//  RGUploadRequest.swift
+//  RGNetwork
+//
+//  Created by RAIN on 2022/6/21.
+//  Copyright © 2022 Smartech. All rights reserved.
+//
+
+import Foundation
+
+class RGUploadRequest {
+
+}
