40aea11a712f2b5ac1b86dc1055e0e519170d464
Smartech <rain_2540@foxmail.com>
2022-07-01 09:39:11 +0800

    chore (project) : add RGNetAddtionalConfig

---
+++ RGNetwork/RGNetwork/Base/RGNetAddtionalConfig.swift
@@ -0,0 +1,15 @@
+//
+//  RGNetAddtionalConfig.swift
+//  RGNetwork
+//
+//  Created by RAIN on 2022/7/1.
+//  Copyright © 2022 Smartech. All rights reserved.
+//
+
+import Foundation
+
+struct RGNetAddtionalConfig {
+
+
+
+}
