f26807321fcff6289433a64bcc959b97ed29845d
Smartech <rain_2540@foxmail.com>
2022-07-05 11:17:49 +0800

    chore (project) : add 'RGNetworkIndicator'

---
+++ RGNetwork/RGNetwork/Tool/RGNetworkIndicator.swift
@@ -0,0 +1,15 @@
+//
+//  RGNetworkIndicator.swift
+//  RGNetwork
+//
+//  Created by RAIN on 2022/7/5.
+//  Copyright © 2022 Smartech. All rights reserved.
+//
+
+import UIKit
+
+public struct RGNetworkIndicator {
+
+
+
+}
