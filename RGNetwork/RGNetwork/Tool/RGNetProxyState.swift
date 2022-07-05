ca2f00f8b9be8b398af98230807bb9a2df2dc831
Smartech <rain_2540@foxmail.com>
2022-07-05 13:24:26 +0800

    chore (project) : add RGNetProxyState

---
+++ RGNetwork/RGNetwork/Tool/RGNetProxyState.swift
@@ -0,0 +1,15 @@
+//
+//  RGNetProxyState.swift
+//  RGNetwork
+//
+//  Created by RAIN on 2022/7/5.
+//  Copyright © 2022 Smartech. All rights reserved.
+//
+
+import Foundation
+
+public struct RGNetProxyState {
+
+
+
+}
