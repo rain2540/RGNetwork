365c764d06b5af6f3b2f172e15d0cb8e506ac15c
Smartech <rain_2540@foxmail.com>
2022-07-05 11:09:47 +0800

    chore (project) : add RGURLHandler

---
+++ RGNetwork/RGNetwork/Tool/RGURLHandler.swift
@@ -0,0 +1,13 @@
+//
+//  RGURLHandler.swift
+//  RGNetwork
+//
+//  Created by RAIN on 2022/7/5.
+//  Copyright © 2022 Smartech. All rights reserved.
+//
+
+import Foundation
+
+struct RGURLHandler {
+
+}
