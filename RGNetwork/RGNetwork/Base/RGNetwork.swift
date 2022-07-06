6844886ef00f437351f01c6012cdd9b7c7a42453
Smartech <rain_2540@foxmail.com>
2022-07-06 11:04:47 +0800

    feat (UIApplication) : add property 'keySceneWindow'

--- RGNetwork/RGNetwork/Base/RGNetwork.swift
+++ RGNetwork/RGNetwork/Base/RGNetwork.swift
@@ -69,35 +69,6 @@ internal extension DispatchQueue {
 }
 
 
-// MARK: - UIApplication Extension
-
-internal extension UIApplication {
-
-  var keySceneWindow: UIWindow? {
-    if #available(iOS 13, *) {
-      var keyWindow: UIWindow?
-      for connectedScene in UIApplication.shared.connectedScenes {
-        guard let windowScene = connectedScene as? UIWindowScene else {
-          continue
-        }
-        if #available(iOS 15, *) {
-          keyWindow = windowScene.keyWindow
-          break
-        } else {
-          for window in windowScene.windows where window.isKeyWindow {
-            keyWindow = window
-          }
-        }
-      }
-      return keyWindow
-    } else {
-      return UIApplication.shared.keyWindow
-    }
-  }
-
-}
-
-
 // MARK: - Debug Log
 
 internal func dLog(
