638155b28e226bd052835a287f331397de84c6ec
Smartech <rain_2540@foxmail.com>
2022-07-06 08:58:01 +0800

    feat (Session) : create download request

--- RGNetwork/RGNetwork/AlamofireExtension/Session+RGNetwork.swift
+++ RGNetwork/RGNetwork/AlamofireExtension/Session+RGNetwork.swift
@@ -51,8 +51,26 @@ extension Session {
     return request
   }
 
+  public func download(
+    urlString: String,
+    method: HTTPMethod = .get,
+    parameters: Parameters? = nil,
+    encoding: ParameterEncoding = URLEncoding.default,
+    headers: HTTPHeaders? = nil,
+    timeoutInterval: TimeInterval = 30.0,
+    destination: DownloadRequest.Destination? = nil
+  ) -> DownloadRequest {
+    let urlPath = RGURLHandler.urlPathString(by: urlString)
+    let request = download(
       urlPath,
+      method: method,
+      parameters: parameters,
+      encoding: encoding,
+      headers: headers,
+      requestModifier: { urlRequest in
+        urlRequest.timeoutInterval = timeoutInterval
       },
+      to: destination)
     return request
   }
 
