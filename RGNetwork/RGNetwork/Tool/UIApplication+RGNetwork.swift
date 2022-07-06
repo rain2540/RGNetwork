d38e69daceeaf46fc23811603cb07f6f371f62b6
Smartech <rain_2540@foxmail.com>
2022-07-06 11:00:04 +0800

    chore (project) : add UIApplication+RGNetwork

---
+++ RGNetwork/RGNetwork/Tool/UIApplication+RGNetwork.swift
@@ -0,0 +1,13 @@
+//
+//  UIApplication+RGNetwork.swift
+//  RGNetwork
+//
+//  Created by RAIN on 2022/7/6.
+//  Copyright © 2022 Smartech. All rights reserved.
+//
+
+import UIKit
+
+extension UIApplication {
+
+}
