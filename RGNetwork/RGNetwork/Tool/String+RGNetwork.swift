72c3bc0eafa60a36af636dcb3f2e1d9667e422d2
Smartech <rain_2540@foxmail.com>
2022-07-06 11:21:24 +0800

    feat (String) : add property 'hasHttpPrefix'

--- RGNetwork/RGNetwork/Tool/String+RGNetwork.swift
+++ RGNetwork/RGNetwork/Tool/String+RGNetwork.swift
@@ -10,6 +10,11 @@ import Foundation
 
 internal extension String {
 
+  /// 是否含有 http / https 前缀
+  var rg_hasHttpPrefix: Bool {
+    return self.hasPrefix("http://") || self.hasPrefix("https://")
+  }
+
   /// 如果含有某个后缀，则删除
   /// - Parameter suffix: 需要删除的后缀
   mutating func rg_removeLast(ifHas suffix: String) {
