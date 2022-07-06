a8d50f39dfe7be3cb4d581c0144bb6d3c9852b98
Smartech <rain_2540@foxmail.com>
2022-07-06 11:18:09 +0800

    feat (String) : add method 'removeLast'

--- RGNetwork/RGNetwork/Base/RGNetwork.swift
+++ RGNetwork/RGNetwork/Base/RGNetwork.swift
@@ -43,13 +43,6 @@ internal extension String {
     return self.hasPrefix("http://") || self.hasPrefix("https://")
   }
 
-  /// 如果含有某个后缀，则删除
-  /// - Parameter suffix: 需要删除的后缀
-  mutating func rg_removeLast(ifHas suffix: String) {
-    if hasSuffix(suffix) {
-      removeLast()
-    }
-  }
 
 }
 
--- RGNetwork/RGNetwork/Tool/String+RGNetwork.swift
+++ RGNetwork/RGNetwork/Tool/String+RGNetwork.swift
@@ -8,6 +8,14 @@
 
 import Foundation
 
-extension String {
+internal extension String {
+
+  /// 如果含有某个后缀，则删除
+  /// - Parameter suffix: 需要删除的后缀
+  mutating func rg_removeLast(ifHas suffix: String) {
+    if hasSuffix(suffix) {
+      removeLast()
+    }
+  }
 
 }
