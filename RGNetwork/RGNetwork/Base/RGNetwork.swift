dc90f4d66582cee9c1511375d0e1b1b0944098ed
Smartech <rain_2540@foxmail.com>
2022-07-06 11:28:33 +0800

    feat (DebugLog) : add function 'dLog'

--- RGNetwork/RGNetwork/Base/RGNetwork.swift
+++ RGNetwork/RGNetwork/Base/RGNetwork.swift
@@ -47,20 +47,4 @@ internal extension String {
 }
 
 
-// MARK: - Debug Log
-
-internal func dLog(
-  _ item: @autoclosure () -> Any,
-  separator: String = " ",
-  terminator: String = "\n",
-  file: String = #file,
-  method: String = #function,
-  line: Int = #line
-) {
-#if DEBUG
-  print("\n/* ***** ***** ***** ***** ***** ***** */\n")
-  print("\(URL(fileURLWithPath: file).lastPathComponent)[\(line)], \(method): \n")
-  print(item(), separator: separator, terminator: terminator)
-  print("")
-#endif
 }
--- RGNetwork/RGNetwork/Tool/DebugLog.swift
+++ RGNetwork/RGNetwork/Tool/DebugLog.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+
+internal func dLog(
+  _ item: @autoclosure () -> Any,
+  separator: String = " ",
+  terminator: String = "\n",
+  file: String = #file,
+  method: String = #function,
+  line: Int = #line
+) {
+#if DEBUG
+  print("\n/* ***** ***** ***** ***** ***** ***** */\n")
+  print("\(URL(fileURLWithPath: file).lastPathComponent)[\(line)], \(method): \n")
+  print(item(), separator: separator, terminator: terminator)
+  print("")
+#endif
+}
