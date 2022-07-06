b9aa0e1a2c52ae9c75f35b8d00e671197acece5a
Smartech <rain_2540@foxmail.com>
2022-07-06 08:56:19 +0800

    feat (Session) : create data request

--- RGNetwork/RGNetwork/AlamofireExtension/Session+RGNetwork.swift
+++ RGNetwork/RGNetwork/AlamofireExtension/Session+RGNetwork.swift
@@ -11,9 +11,23 @@ import Alamofire
 
 extension Session {
 
+  public func request(
+    urlString: String,
+    method: HTTPMethod = .get,
+    parameters: Parameters? = nil,
+    encoding: ParameterEncoding = URLEncoding.default,
+    headers: HTTPHeaders? = nil,
+    timeoutInterval: TimeInterval = 30.0
+  ) -> DataRequest {
+    let urlPath = RGURLHandler.urlPathString(by: urlString)
     let request = request(
       urlPath,
+      method: method,
+      parameters: parameters,
+      encoding: encoding,
+      headers: headers,
       requestModifier: { urlRequest in
+        urlRequest.timeoutInterval = timeoutInterval
       })
     return request
   }
